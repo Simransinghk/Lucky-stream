6a3546715afda3f7cab684732e19c52a82887bad
Mr Lucky <94621036+Mrvk1703@users.noreply.github.com>
2022-03-17 09:47:02 +0530

    Update thumbnail.py

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -129,3 +129,10 @@ async def gen_thumb(videoid):
             (255, 255, 255),
             font=arial,
         )
+        try:
+            os.remove(f"cache/thumb{videoid}.png")
+        except:
+            pass
+        background.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception:
