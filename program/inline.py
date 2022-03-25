e7edaa9f18868c7d2ff66c018f4a072eac7907c5
Mr Lucky <94621036+Mrvk1703@users.noreply.github.com>
2022-03-25 22:33:41 +0530

    Update inline.py

--- program/inline.py
+++ program/inline.py
@@ -25,43 +25,63 @@ from pyrogram.types import (
 )
 from youtubesearchpython import VideosSearch
 
+answer = []
 
-@Client.on_inline_query()
-async def inline(client: Client, query: InlineQuery):
-    answers = []
-    search_query = query.query.lower().strip().rstrip()
-
-    if search_query == "":
-        await client.answer_inline_query(
-            query.id,
-            results=answers,
-            switch_pm_text="Type the YouTube video name to search !",
-            switch_pm_parameter="help",
-            cache_time=0,
-        )
-    else:
-        search = VideosSearch(search_query, limit=50)
-
-        for result in search.result()["result"]:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description="{}, {} views.".format(
-                        result["duration"], result["viewCount"]["short"]
-                    ),
-                    input_message_content=InputTextMessageContent(
-                        "🔗 https://www.youtube.com/watch?v={}".format(result["id"])
-                    ),
-                    thumb_url=result["thumbnails"][0]["url"],
-                )
-            )
-
-        try:
-            await query.answer(results=answers, cache_time=0)
-        except errors.QueryIdInvalid:
-            await query.answer(
-                results=answers,
-                cache_time=0,
-                switch_pm_text="error: search timed out",
-                switch_pm_parameter="",
-            )
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pause Stream",
+            description=f"Pause the current playout on group call.",
+            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Resume Stream",
+            description=f"Resume the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Mute Stream",
+            description=f"Mute the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
+            input_message_content=InputTextMessageContent("/mute"),
+        ),
+        InlineQueryResultArticle(
+            title="Unmute Stream",
+            description=f"Unmute the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
+            input_message_content=InputTextMessageContent("/unmute"),
+        ),
+        InlineQueryResultArticle(
+            title="Skip Stream",
+            description=f"Skip to next track. | For Specific track number: /skip [number] ",
+            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="End Stream",
+            description="Stop the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("/stop"),
+        ),
+        InlineQueryResultArticle(
+            title="Shuffle Stream",
+            description="Shuffle the queued tracks list.",
+            thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="Seek Stream",
+            description="Seek the ongoing stream to a specific duration.",
+            thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
+            input_message_content=InputTextMessageContent("/seek 10"),
+        ),
+        InlineQueryResultArticle(
+            title="Loop Stream",
+            description="Loop the current playing music. | Usage: /loop [enable|disable]",
+            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
